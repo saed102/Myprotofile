923b41cd55e022e00ac89c3debd292927f0918e4
Mohamed Saed <saed8804@gmail.com>
2023-01-25 11:52:45 +0200

    saed

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -12,5 +12,5 @@ class Utilty {
   static Future<void> openMyLocation() =>
       openUrl("https://www.google.com/maps/place/29%C2%B058'56.6%22N+31%C2%B019'26.0%22E/@29.982382,31.321686,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0xfb111231089c9d07!8m2!3d29.982382!4d31.3238747?hl=en");
   static Future<void> openMyPhoneNo() => openUrl("tel:+201146071874");
-  static Future<void> openWhatsapp() => openUrl("https://wa.me/01146071874");
+  static Future<void> openWhatsapp() => openUrl("https://wa.me/+201146071874");
 }
